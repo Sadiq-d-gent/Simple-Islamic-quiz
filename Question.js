1caa57502d9496f9398bd3509326a1a7a8e3ae0e
Sadiq-d-gent <138960197+Sadiq-d-gent@users.noreply.github.com>
2023-11-25 17:33:37 +0100

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,289 @@
+let questions = [
+  // {
+  //   num: 1,
+  //   question: "What does HTML stands for?",
+  //   answer: "C. Hyper Text Markup Language",
+  //   options: [
+  //     "A. Hyper Type Multi Language",
+  //     "B. Hyper Text Multi Language",
+  //     "C. Hyper Text Markup Language",
+  //     "D. Home Text Multi Language",
+  //   ],
+  // },
+  // {
+  //   num: 2,
+  //   question: "What CSS stand for?",
+  //   answer: "A. Cascading Style Sheet",
+  //   options: [
+  //     "A. Cascading Style Sheet",
+  //     "B. Cute Style Sheet",
+  //     "C. Computer Style Sheet",
+  //     "D. Counting Style Sheet",
+  //   ],
+  // },
+
+  // {
+  //   num: 3,
+  //   question: "What does PHP stand for?",
+  //   answer: "A. Hypertext Preprocessor",
+  //   options: [
+  //     "A. Hypertext Preprocessor",
+  //     "B. Hometext Programming",
+  //     "C. Hypertext Preprogramming",
+  //     "D. Programming Hypertext Preprocessor",
+  //   ],
+  // },
+  // {
+  //   num: 4,
+  //   question: "What does SQL stand for?",
+  //   answer: "D. Structured Query Language",
+  //   options: [
+  //     "A. Strength Query Language",
+  //     "B. Stylesheet Query Language",
+  //     "C. Science Query Language",
+  //     "D. Structured Query Language",
+  //   ],
+  // },
+  // {
+  //   num: 5,
+  //   question: "What does XML stand for?",
+  //   answer: "D. Extensible Markup Language",
+  //   options: [
+  //     "A. Excellent Multiple Language",
+  //     "B. Explore Multiple Language",
+  //     "C. Extra Markup language",
+  //     "D. Extensible Markup Language",
+  //   ],
+  // },
+
+     
+  {
+      num: 1,
+      question: "Who is called a companion of the Prophet Muhammad (PBUH)?",
+      answer: "A. A companion is one who saw the Prophet Muhammad (PBUH) and believed in him and died as a Muslim",
+      options: [
+        "A. A companion is one who saw the Prophet Muhammad (PBUH) and believed in him and died as a Muslim",
+        "B. A companion is one who is a Muslim in a modern era",
+        "C. All Muslims are considered as companions",
+        "D. No one is a companion",
+      ],
+    },
+
+    {
+      num: 2,
+      question: "Which of the companions did Prophet Muhammad (PBUH) state as the person who accepted Islam without even a little hesitation?",
+      answer: "D. Abu Bakr Al-Siddiq",
+      options: [
+        "A. Omar Bin Al-Khattab",
+        "B. Ali Bin Abu Talib",
+        "C. Uthman Bin Affan",
+        "D. Abu Bakr Al-Siddiq",
+      ],
+    },
+
+    {
+      num: 3,
+      question: "Who is the fourth of the rightly-guided Caliph after the life of Prophet Muhammad (PBUH)?",
+      answer: "C. Ali Bin Abu Talib",
+      options: [
+        "A. Uthman Bin Affan",
+        "B. Omar Bin Al-Khattab",
+        "C. Ali Bin Abu Talib",
+        "D. Abu Bakr Al-Siddiq",
+      ],
+    },
+
+    {
+      num: 4,
+      question: "Which of the companions was a paternal uncle of the Prophet, and was considered as a strong personality both in character and in physique?",
+      answer: "A. Abu Talib",
+      options: [
+        "A. Abu Talib",
+        "B. Hamzah lbn Abdul Muttalib",
+        "C. Abdul Muttalib",
+        "D. None of the above"
+      ],
+    },
+
+    {
+      num: 5,
+      question: "How many of the companions have been promised paradise?",
+      answer: "C. Ten",
+      options: [
+        "A. One thousand",
+        "B. One hundred",
+        "C. Ten",
+        "D. One"
+      ],
+    },
+
+    {
+      num: 6,
+      question: "Which companion of the Prophet Muhammad (PBUH) is called “The Possessor of Two Lights”?",
+      answer: "C. Uthman Bin Affan",
+      options: [
+        "A. Abu Bakr Al-Siddiq",
+        "B. Ali Bin Abu Talib",
+        "C. Uthman Bin Affan",
+        "D. Omar Bin Al-Khattab"
+      ],
+    },
+
+    {
+      num: 7,
+      question: " Which of the companions did Prophet Muhammad (PBUH) enjoin the responsibility of Adhan, the person who called out Adhan standing atop the Kaabah?",
+      answer: "B. Bilal Ibn Rabah",
+      options: [
+        "A. Ammar Ibn Yasser",
+        "B. Bilal Ibn Rabah",
+        "C. Abdullah Ibn Masood",
+        "D. None of the above"
+      ],
+    },
+
+    {
+      num: 8,
+      question: "Who were considered as the closest companions of the Prophet Muhammad (PBUH)?",
+      answer: "A. Abu Bakr Al-Siddiq and Omar Bin Al-Khatab",
+      options: [
+        "A. Abu Bakr Al-Siddiq and Omar Bin Al-Khatab",
+        "B. Abdullah Ibn Masood and Abdullah Ibn Umar",
+        "C. Abdulrahman Ibn Awuf and Bilal Ibn Rabah",
+        "D. Khalid Ibn Al-Waleed and Salman Al-Faresy"
+      ],
+    },
+
+    {
+      num: 9,
+      question: "Of which companion did the Prophet say, “If there was to be a Prophet after Me, it would have been you”?",
+      answer: "D. Abu Bakr Al-Siddiq",
+      options: [
+        "A. Omar Bin Al-Khatab",
+        "B. Uthman Bin Affan",
+        "C. Ali Bin Abu Talib",
+        "D. Abu Bakr Al-Siddiq"
+      ],
+    },
+
+    {
+      num: 10,
+      question: "Who is the first martyr in Islam?",
+      answer: "D. Khadeejah",
+      options: [
+        "A. Sumayyah",
+        "B. Fathima",
+        "C. Ayesha",
+        "D. Khadeejah"
+      ],
+    },
+    {
+      num: 11,
+      question: "What is the single most important message from Allah that all the messengers brought?",
+      answer: "B. The most important message was to worship Allah alone and none else",
+      options: [
+        "A. The most important message was to eat, drink and to remain happy",
+        "B. The most important message was to worship Allah alone and none else",
+        "C. The most important message was to worship all idols and deities",
+        "D. The most important message was that they were messengers of Allah"
+      ],
+    },
+    {
+      num: 12,
+      question: " Who is the last Messenger of Allah?",
+      answer: "C. Prophet Muhammad (PBUH)",
+      options: [
+        "A. Prophet Jesus (PBUH)",
+        "B. Prophet Abraham (PBUH)",
+        "C. Prophet Muhammad (PBUH)",
+        "D. Prophet Moses (PBUH)"
+      ],
+    },
+    {
+      num: 13,
+      question: "Who has Allah mentioned in the Quran as the best example to follow?",
+      answer: "A. Prophet Muhammad (PBUH)",
+      options: [
+        "A. Prophet Muhammad (PBUH)",
+        "B. Pharaoh",
+        "C. Angels",
+        "D. Jinn"
+      ],
+    },
+    {
+      num: 14,
+      question: "Which prophet has been mentioned in the Quran as a friend of Allah?",
+      answer: "C. Prophet Abraham (PBUH)",
+      options: [
+        "A. Prophet Muhammad (PBUH)",
+        "B. Prophet Jesus (PBUH)",
+        "C. Prophet Abraham (PBUH)",
+        "D. Prophet Moses (PBUH)"
+      ],
+    },
+    {
+      num: 15,
+      question: "To which prophet did Allah order to build an ark?",
+      answer: "B. Prophet Noah (PBUH)",
+      options: [
+        "A. Prophet Jesus (PBUH)",
+        "B. Prophet Noah (PBUH)",
+        "C. Prophet Abraham (PBUH)",
+        "D. Prophet David (PBUH)"
+      ],
+    },
+    {
+      num: 16,
+      question: "How many prophets are mentioned in the Quran?",
+      answer: "D. 25",
+      options: [
+        "A. 29",
+        "B. 28",
+        "C. 26",
+        "D. 25"
+      ],
+    },
+    {
+      num: 17,
+      question: "To which prophet did Allah directly speak to?",
+      answer: "D. Prophet Moses (PBUH)",
+      options: [
+        "A. Prophet Jesus (PBUH)",
+        "B. Prophet Abraham (PBUH)",
+        "C. Prophet David (PBUH)",
+        "D. Prophet Moses (PBUH)"
+      ],
+    },
+    {
+      num: 18,
+      question: "Who has been sent as a mercy to the worlds?",
+      answer: "B. Prophet Muhammad (PBUH)",
+      options: [
+        "A. Prophet Jesus (PBUH)",
+        "B. Prophet Muhammad (PBUH)",
+        "C. Prophet Moses (PBUH)",
+        "D. Prophet Abraham (PBUH)"
+      ],
+    },
+    {
+      num: 19,
+      question: "Which of the prophets were ordered by Allah to build the Kaabah in Makkah?",
+      answer: "D. Prophet Abraham (PBUH) and Prophet Ishmael (PBUH)",
+      options: [
+        "A. Prophet Adam (PBUH) and Prophet Noah (PBUH)",
+        "B. Prophet Jesus (PBUH) and Prophet Muhammad (PBUH)",
+        "C. Prophet Moses (PBUH) and Prophet David (PBUH)",
+        "D. Prophet Abraham (PBUH) and Prophet Ishmael (PBUH)"
+      ],
+    },
+    {
+      num: 20,
+      question: "Which prophet was born miraculously without the intervention of a man?",
+      answer: "D. Prophet Jesus (PBUH) son of Mary",
+      options: [
+        "A. Prophet Abraham",
+        "B. Prophet Moses",
+        "C. Prophet Noah",
+        "D. Prophet Jesus (PBUH) son of Mary"
+      ],
+    },
+];
